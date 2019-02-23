563428e04ecf1a9cdfd0504759ea0be25b4c00b4
Rob Jenks <robjenks@gmail.com>
2019-02-23 23:53:42 +0000

    Implement gradient descent solver for linear regression

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
-import regression_gd
+import gradient_descent
 import iris_classifier
 
 
 def content():
     return [
         # Linear regression via gradient descent and minimising squared error cost function
-        ('regression-gd', regression_gd.run_linear_regression_gradient_descent),
+        ('gradient-descent', gradient_descent.run_linear_regression_gradient_descent),
 
         # Supervised learning models applied to iris classification dataset
         ('iris-classification', iris_classifier.iris_classification)
