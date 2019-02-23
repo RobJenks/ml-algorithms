e7d260a3858ea0ecc32db582e638e68c91610238
Rob Jenks <robjenks@gmail.com>
2019-02-23 20:52:27 +0000

    Initial project commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import regression_gd
+import iris_classifier
+
+
+def content():
+    return [
+        # Linear regression via gradient descent and minimising squared error cost function
+        ('regression-gd', regression_gd.linear_regression_gradient_descent),
+
+        # Supervised learning models applied to iris classification dataset
+        ('iris-classification', iris_classifier.iris_classification)
+    ]
+
+
+def main():
+    for id, process in content():
+        if id in sys.argv:
+            process()
+
+
+# Entry point
+if __name__ == "__main__":
+    main()
